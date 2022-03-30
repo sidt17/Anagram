f8ffe21d1745b9be86f2e7fc4f032f0b4dd09fcd
Siddhant <santoshshrivastava39@gmail.com>
2022-03-30 10:44:13 +0530

    Anagram Ques Solved

---
+++ Anagram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class Anagram
+    {
+        public void AnagramQues(string str1, string str2)
+        {
+            
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("It is a Anagram");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Anagram");
+            }
+        }
+       
+        
+          
+        
+         
+    }
+   
+}
+  
